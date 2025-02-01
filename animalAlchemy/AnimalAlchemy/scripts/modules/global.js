7f72f18339c441b3bcb9de6051eafe5058e00894
ccKatiii <cc241053@fhstp.ac.at>
2025-02-01 22:26:06 +0100

    Add files via upload

---
+++ animalAlchemy/AnimalAlchemy/scripts/modules/global.js
@@ -0,0 +1,66 @@
+
+
+const global = {};
+
+global.canvas = document.querySelector("#canvas");
+global.ctx = canvas.getContext("2d");
+global.canvas.width = 1208;
+global.canvas.height = 804;
+global.prevTotalRunningTime = 0;
+global.deltaTime = 0;
+global.allGameObjects = [];
+global.playerObject = {};
+global.exit = [];
+global.boarder = [];
+global.animals = [];
+global.collectedAnimals = [null, null, null, null];
+global.breedingM = [];
+global.collidingObjects = [];
+global.animalsOnMachine = [];
+global.popUp = {};
+global.allAnimalsImages = [];
+global.gamePaused = false;
+global.animalCollectionList = [];
+
+
+global.getCanvasBounds = function () {
+    let bounds =  {
+        "left": 0,
+        "right": this.canvas.width,
+        "top": 0, 
+        "bottom": this.canvas.height
+    }
+
+    return bounds;
+}
+
+global.checkCollisionWithAnyOther = function (givenObject) {
+    for (let i = 0; i < global.allGameObjects.length; i++) {
+        let otherObject = global.allGameObjects[i];
+        if (otherObject.active == true) {
+            let collisionHappened = this.detectBoxCollision(givenObject, otherObject);
+            if (collisionHappened) {
+                givenObject.reactToCollision(otherObject);
+                otherObject.reactToCollision(givenObject);
+            }
+        }
+    }
+}
+
+global.detectBoxCollision = function (gameObject1, gameObject2) {
+    let box1 = gameObject1.getBoxBounds();
+    let box2 = gameObject2.getBoxBounds();
+    if (gameObject1 != gameObject2) {
+        if (box1.top <= box2.bottom && 
+            box1.left <= box2.right && 
+            box1.bottom >= box2.top &&
+            box1.right >= box2.left)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+export { global }
